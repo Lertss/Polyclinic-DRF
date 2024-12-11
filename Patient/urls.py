c7470814b86a0e937bb076c646d3ed494b191b03
Serhii <lerojserg@gmail.com>
2024-12-11 01:05:59 +0100

    Added url from djoser

--- Patient/urls.py
+++ Patient/urls.py
@@ -1,3 +1,12 @@
-urlpatterns = [
+from django.contrib import admin
+from django.urls import re_path, include
 
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+
+urlpatterns = [
+    # path("", include(router.urls)),
+    re_path(r'^auth/', include('djoser.urls')),
+    re_path(r'^auth/', include('djoser.urls.authtoken')),
 ]
