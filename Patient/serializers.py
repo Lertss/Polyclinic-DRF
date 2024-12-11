a8eed82e0f3ebad1fcd12822942f3d619290f9b8
Serhii <lerojserg@gmail.com>
2024-12-11 01:06:24 +0100

    Added PatientSerializer and update CustomUserCreatePasswordRetypeSerializer

--- Patient/serializers.py
+++ Patient/serializers.py
@@ -1,4 +1,6 @@
 from djoser.serializers import UserCreatePasswordRetypeSerializer
+from rest_framework import serializers
+from .models import Patient
 
 
 class CustomUserCreatePasswordRetypeSerializer(UserCreatePasswordRetypeSerializer):
@@ -8,4 +10,42 @@ class CustomUserCreatePasswordRetypeSerializer(UserCreatePasswordRetypeSerialize
                   'email',
                   'first_name',
                   'last_name',
-                  'password',]
+                  'phone_number',
+                  'gender',
+                  'birth_date',
+                  'password',
+                  ]
+
+
+
+class PatientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Patient
+        fields = [
+            'id',
+            'user',
+            'region',
+            'neighborhood',
+            'city',
+            'street',
+            'house',
+            'apartment',
+            'allergy',
+            'blood_type',
+            'medical_insurance_number',
+        ]
+
+    def create(self, validated_data):
+        """
+        Custom behavior for creating a Patient instance.
+        """
+        return Patient.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Custom behavior for updating a Patient instance.
+        """
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
